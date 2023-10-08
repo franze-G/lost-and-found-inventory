4e6b0105cca9e61f8e7514d32fc7e727169741b2
Franze <franzelighil.garcia@adamson.edu.ph>
2023-10-08 18:09:00 +0800

    login

---
+++ login/login.php
@@ -0,0 +1,34 @@
+<?php 
+
+?>
+
+<html lang="en">
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="log.css">
+    </head>
+    
+    <body>
+        <div class="container"> 
+        <img class="logo" src="adam.png" alt="Adamson">
+
+            <div class="form-box">  
+                <form>
+                    <div class="input-group">
+                        <div class="input-field">
+                            <input type="number" placeholder="ID Number">
+                        </div>
+
+                        <div class="input-field">
+                            <input type="password" placeholder="Password">
+                        </div>
+                        <p>Forgot password <a href="#">click here</a></p>
+                    </div>
+                    <div class="btn-field">
+                        <button type="button">Login</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
