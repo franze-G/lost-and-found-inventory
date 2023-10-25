46b0cec777999d7bf76ff72f985c390b365c69ba
Franze <franzelighil.garcia@adamson.edu.ph>
2023-10-25 10:45:39 +0800

    register

---
+++ dashboard/dashboard.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="dash.css">
+</head>
+<body>
+    <div class="container">
+        <ul class="nav">
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#">Inventory</a></li>
+        <li><a href="#">Retrieve Item</a></li>
+        <li><a href="#">Register Item</a></li>
+        </ul>
+        <div class="lostitems">
+            <h1 class="lost">Lost items</h1>
+        </div>
+        <div class="founditems">
+            <h1 class="found">Found items</h1>
+        </div>
+        <div class="search">
+            <h1 class="searchitm">Search item</h1>
+        </div>
+    </div>
+</body>
---
+++ register/register.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Retrieve Item</title>
+    <link rel="stylesheet" href="regist.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            
+        </div>
+        <div class="menu">
+        </div>
+        <div class="search">
+            <h1 class="searchitm">Retrieve Item </h1>
+            <div class="input-field">
+                <input type="text" name="fullname" id="fullname" placeholder="Full name">
+            </div>
+            <div class="input-field">
+                <input type="text" name="studentnum" id="studentnum" placeholder="Student number">
+            </div>
+            <div class="input-field">
+                <input type="text" name="course" id="Course" placeholder="Course">
+            </div>
+
+            <div class="input-field">
+                <input type="dropdown" name="category" id="category" placeholder="Category of Item">
+            </div>
+            <div class="input-field">
+                <input type="text" name="type" id="type" placeholder="Type of item">
+            </div>
+            <div class="input-field">
+                <input type="text" name="itemname" id="itemname" placeholder="Item name">
+            </div>
+
+            
+            <div class="input-field">
+                <input type="text" name="name" id="fullname" placeholder="Item color">
+            </div>
+            <div class="input-field">
+                <input type="date" name="lostdate" id="lostdate" placeholder="Date of Lost">
+            </div>
+            <div class="input-field">
+                <input type="date" name="registerdate" id="registerdate" placeholder="Date registered">
+            </div>
+
+            <div class="input-field">
+                <input type="date" name="claimdate" id="claimdate" placeholder="Date of claim">
+            </div>    
+            <div class="btn-field">
+                    <button type="submit">Submit</button>
+            </div>  
+        </div>
+    </div>
+</body>
